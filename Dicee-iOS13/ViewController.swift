39b403a79f56613b8f47dc828c8d7e67ac50d084
Luka Leko <luka_dl@hotmail.com>
2023-02-16 22:41:21 -0500

    Initial iOS Dicee app from AppBrewery commit.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI element. Hold Ctrl + drag the ImageView into the line.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Changing the image on the two dice on the screen.
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceThree")
+        
+        // Example of changing the alpha property
+        //diceImageView1.alpha = 0.5
+        
     }
 
 
